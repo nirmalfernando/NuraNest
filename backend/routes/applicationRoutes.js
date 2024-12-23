53d89611280873f909320e924219bed948c4e756
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-12-23 12:39:01 +0530

    Add token verification middleware to application routes

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -6,22 +6,23 @@ const {
   deleteApplication,
 } = require("../controllers/application.js");
 const express = require("express");
+const { verifyToken } = require("../middlewares/authRole.js");
 
 const router = express.Router();
 
 // Create a new Application
-router.post("/", createApplication);
+router.post("/", verifyToken, createApplication);
 
 // Update an existing Application
-router.put("/:id", updateApplication);
+router.put("/:id", verifyToken, updateApplication);
 
 // Get an Application by ID
-router.get("/:id", getApplicationById);
+router.get("/:id", verifyToken, getApplicationById);
 
 // Get all Applications
-router.get("/", getApplications);
+router.get("/", verifyToken, getApplications);
 
 // Delete an Application
-router.delete("/:id", deleteApplication);
+router.delete("/:id", verifyToken, deleteApplication);
 
 module.exports = router;
